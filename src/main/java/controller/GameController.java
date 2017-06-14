02c7790bd1025afb0aa99b67b9a873c796f95b74
mari <maristein3@gmail.com>
2017-06-14 10:31:17 +0200

    Change game mechanics

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -22,15 +22,14 @@ public class GameController {
 
         while (!game.getBoard().hasWon() && !isDraw) {
 
-
             try {
-                game.changePlayer();
                 x = Ui.selectFromList(3, "Select y position for " + game.getCurrentPlayer() + ": ");
                 y = Ui.selectFromList(3, "Select x position: " + game.getCurrentPlayer() + ": ");
                 game.updateGameState(game.getCurrentPlayer(), x, y);
                 game.getBoard().hasWon();
+                game.changePlayer();
                 moveCounter += 1;
-                if(moveCounter == 9 && !game.getBoard().hasWon()) {
+                if (moveCounter == 9 && !game.getBoard().hasWon()) {
                     isDraw = true;
                     game.setCurrentState(GameState.DRAW);
                 }
@@ -42,9 +41,28 @@ public class GameController {
             Ui.printHeader("Crosses and Noughts Game");
             Ui.printBoard(game.getBoard().getTable());
 
+            if (game.getBoard().hasWon()) {
+                game.changePlayer();
+                System.out.println("WIN! " + game.getCurrentPlayer());
+                GameController.playAgain(game);
 
+            }
+            if (moveCounter == 9 && !game.getBoard().hasWon()) {
+                System.out.println("Draw");
+                GameController.playAgain(game);
+            }
         }
-        if(game.getBoard().hasWon()) System.out.println("WIN! " + game.getCurrentPlayer());
-        else System.out.println("Draw");
+    }
+
+    private static void playAgain(Game game) {
+        String playOrnot = Ui.userInputString("Do you want to play again? (y/n): ");
+        if (playOrnot.equals("y")) {
+            Ui.clear();
+            game.initGame();
+            Ui.printHeader("Crosses and Noughts Game");
+            Ui.printBoard(game.getBoard().getTable());
+        }
+
     }
 }
+
